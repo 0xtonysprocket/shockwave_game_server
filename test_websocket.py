a595f3983a5410dfd48c87f83cb21f6aacf34aba
Tony Sprocket <tonysprocket@gmail.com>
2022-10-01 21:48:55 -0600

    websocket working properly

---
+++ test_websocket.py
@@ -0,0 +1,36 @@
+
+import websocket
+import _thread
+import time
+import rel
+
+addr = "ws://127.0.0.1:12345/join_game"
+
+
+def on_message(ws, message):
+    print(message)
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+
+def on_open(ws):
+    print("Opened connection")
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(addr,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+
+    ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
